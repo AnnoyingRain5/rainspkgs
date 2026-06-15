9d7a97bd59ab8944cc140eeab8fe2185c24d2897
AnnoyingRains <annoyingrain5@gmail.com>
2026-06-15 16:33:58 +1000

    fetchzip instead of fetchTarball

--- pkgs/proton-ge-gdk/default.nix
+++ pkgs/proton-ge-gdk/default.nix
@@ -1,4 +1,4 @@
-{ pkgs }:
+{ pkgs, fetchzip }:
 
 (pkgs.proton-ge-bin.override {
   steamDisplayName = "GE-Proton-GDK";
@@ -7,7 +7,7 @@
     finalAttrs: _: {
       pname = "proton-ge-gdk-bin";
       version = "GE-Proton10-32";
-      src = fetchTarball {
+      src = fetchzip {
         url = "https://github.com/Weather-OS/GDK-Proton/releases/download/release10-32/GDK-Proton10-32.tar.gz";
         sha256 = "sha256:03b1f9y61j9a4mqav6g5wxgmncicv1a9cd76xdjigzir8a5fx8n7";
       };

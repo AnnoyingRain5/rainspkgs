f33e3d8085372ab422011c456c58cf6e7d6b1924
AnnoyingRains <annoyingrain5@gmail.com>
2026-07-14 18:40:55 +1000

    update monado again

--- pkgs/monado-pimax/default.nix
+++ pkgs/monado-pimax/default.nix
@@ -10,8 +10,8 @@ monado.overrideAttrs (oldAttrs: {
     domain = "gitlab.freedesktop.org";
     owner = "AnnoyingRain5";
     repo = "monado";
-    rev = "7c56b5c1723a223fb149e4d01967558c407e560e";
-    hash = "sha256-tAG22s/RDvOkSd5LL30W9iu7KvEWBlA0+SDHwmipfTw=";
+    rev = "3b082ccf97f4a5ea16147fc279729897a65340f5";
+    hash = "sha256-tJUAwVwyVDEUgfQZj38SHXDUs7dmVgv6u9GV4eoOpBA=";
   };
   patches = builtins.filter (
     patch: patch.name != "monado-cylinder-aspectRatio.patch"

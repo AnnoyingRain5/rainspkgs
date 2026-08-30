94e63b13189cf6dfcc8a08496ba93545bbaced5e
AnnoyingRains <annoyingrain5@gmail.com>
2026-08-30 17:15:54 +1000

    Change derivation slightly to force rebuild (???)

--- pkgs/cachyos-pimax-kernel/default.nix
+++ pkgs/cachyos-pimax-kernel/default.nix
@@ -6,7 +6,7 @@
 }:
 
 let
-  kernel = cachyosKernels.linux-cachyos-latest-lto-x86_64-v3.override {
+  kernel = cachyosKernels.linux-cachyos-latest-lto-x86_64.override {
     pname = "cachyos-pimax-kernel";
     lto = "thin";
 
